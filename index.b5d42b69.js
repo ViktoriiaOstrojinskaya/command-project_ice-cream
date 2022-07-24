ca6fce3a4a746798fa565d70520fc4062cdd4efb
shvaiko <102625907+shvaiko@users.noreply.github.com>
2022-07-24 19:24:59 +0000

    Deploying to gh-pages from @ shvaiko/ice-cream@43823064dedda8e50c612f997b9dc51330594c87 🚀

---
+++ index.b5d42b69.js
@@ -0,0 +1,2 @@
+!function(){var e={openMenuBtn:document.querySelector("[data-modal-open]"),closeMenuBtn:document.querySelector("[data-modal-close]"),menu:document.querySelector("[data-modal]")};function n(){e.menu.classList.toggle("is-hidden")}e.openMenuBtn.addEventListener("click",n),e.closeMenuBtn.addEventListener("click",n)}();
+//# sourceMappingURL=index.b5d42b69.js.map
