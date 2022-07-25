47d6767434f7f716979264ba9c953dfcc68a2275
nonbinary00 <agarkovaalysh12@gmail.com>
2022-07-25 19:01:03 +0200

    not open

--- src/js/contact.js
+++ src/js/contact.js
@@ -1,31 +1,31 @@
-// (() => {
-//     const refs = {
-//       openMenuBtn: document.querySelector('[data-modal-open]'),
-//       closeMenuBtn: document.querySelector('[data-modal-close]'),
-//       menu: document.querySelector('[data-modal]'),
-//     };
-//     refs.openMenuBtn.addEventListener('click', toggleModal);
-//     refs.closeMenuBtn.addEventListener('click', toggleModal);
-//     function toggleModal() {
-//       refs.menu.classList.toggle('is-hidden');
-//     }
-//   })();
-
-  (() => {
+(() => {
     const refs = {
-      openMenuBtn: document.querySelector('.contacts-btn__location'),
-      closeMenuBtn: document.querySelector('.map__btn'),
-      menu: document.querySelector('.contacts__map'),
+      openMenuBtn: document.querySelector('[data-modal-open]'),
+      closeMenuBtn: document.querySelector('[data-modal-close]'),
+      menu: document.querySelector('[data-modal]'),
     };
-  
-    refs.openMenuBtn.addEventListener('click', toggleMenu);
-    refs.closeMenuBtn.addEventListener('click', toggleMenu);
-  
-    function toggleMenu() {
+    refs.openMenuBtn.addEventListener('click', toggleModal);
+    refs.closeMenuBtn.addEventListener('click', toggleModal);
+    function toggleModal() {
       refs.menu.classList.toggle('is-hidden');
-      document.body.classList.toggle('map-open');
     }
   })();
+
+// (() => {
+//   const refs = {
+//     openMenuBtn: document.querySelector('.contacts-btn__location'),
+//     closeMenuBtn: document.querySelector('.map__btn'),
+//     menu: document.querySelector('.backdrop__location'),
+//   };
+
+//   refs.openMenuBtn.addEventListener('click', toggleMenu);
+//   refs.closeMenuBtn.addEventListener('click', toggleMenu);
+
+//   function toggleMenu() {
+//     refs.menu.classList.toggle('is-hidden');
+//     document.body.classList.toggle('map-open');
+//   }
+// })();
 
 // (() => {
 //   const refs = {
